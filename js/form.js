56e1125fc15e9859c1a660d6f99bbaf852c38f88
Oleg Lytvyn <igocooper@gmail.com>
2016-07-11 12:01:08 +0000

    btn animation added

--- js/form.js
+++ js/form.js
@@ -1,14 +1,13 @@
 $(function(){
 $('#btn_send').click(function(e){
     e.preventDefault();
-    
     $.ajax({
         url: 'https://formspree.io/igocooper@gmail.com',
         method: 'POST',
         data: { email: $('#email').val()},
         data_type: 'json'
     }).done(function(){
-        alert('Thank you!');
+        alert('Thanks');
     });
 }) ;
 });
